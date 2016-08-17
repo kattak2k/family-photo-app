e337106e6c8ce3a7fec947f5a52e554fccd03fb4
Kiran K <kkiran.ror@gmail.com>
2016-08-17 04:44:58 +0000

    Add payment input options

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, 
          :confirmable,   #Ins Katta
          :recoverable, :rememberable, :trackable, :validatable
+  #Ins Start Katta       
+  has_one :payment
+  #By default nested attribute updating is turned off and you can enable it using the #accepts_nested_attributes_for class method
+  accepts_nested_attributes_for :payment  
+  #Ins End Katta
 end
 
